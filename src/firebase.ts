d6aeb4c28d7b7ee6e5b56e687b842612311a5a29
mattyflip <mattyflipdesigns@gmail.com>
2026-05-05 00:16:10 -0400

    security: Move Firebase configuration to environment variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAtuguwWyHdSbRzv5lyVkyXEIhZgyOeiSw",
-  authDomain: "ebk-master-diagnostic-portal.firebaseapp.com",
-  projectId: "ebk-master-diagnostic-portal",
-  storageBucket: "ebk-master-diagnostic-portal.firebasestorage.app",
-  messagingSenderId: "1044497196393",
-  appId: "1:1044497196393:web:e191e8f2eae25b20e789f8",
-  measurementId: "G-VDYTTPZYN4"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
